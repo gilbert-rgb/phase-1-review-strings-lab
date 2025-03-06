838e7b1434bd330762e26ca464c008c4c59438ca
Gilbert Icheboi <gilbert.icheboi@student.moringaschool>
2025-03-06 02:58:55 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+// helpers.js
+const currentUser = "Gillychebson";  // The name can be changed to test different cases
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
